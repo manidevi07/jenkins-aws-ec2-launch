0f347b5da60e4d68208002c37290e6a881e0c3af
usman6745 <33342456+usman6745@users.noreply.github.com>
2019-03-13 18:19:37 +0530

    Update ec2.sh

--- ec2.sh
+++ ec2.sh
@@ -18,12 +18,9 @@ region_name=$5
 Key_Name=$6
 Key_Name_Value=$7
 
-//launchid=$6
-
-
 launch_ec2()
 {
-echo -e "$ami_id\n$keypair_name\n$Instance_type\n$subnetid\n$region_name"
+echo -e "$ami_id\n$keypair_name\n$Instance_type\n$subnetid\n$region_name\n$Key_Name\n$Key_Name_Value"
 
 aws ec2 run-instances --image-id $ami_id --count 1 --instance-type $Instance_type --key-name $keypair_name --security-group-ids sg-088974251af4f8415 --subnet-id $subnetid --region $region_name > ec2.txt
 }
@@ -33,8 +30,6 @@ instancespec()
 sleep 3
 grep InstanceId ec2.txt | tr -d '", "' > InstanceId
 sed -i 's/:/=/g' InstanceId
-launchid=$( cat InstanceId )
-echo "instance id is $lanunchid"
 echo "aws ec2 create-tags --resources ""$"InstanceId" --tags Key=$Key_Name,Value=$Key_Name_Value --region $region_name" >> InstanceId
 chmod +x InstanceId
 ./InstanceId
